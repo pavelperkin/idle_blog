035659e82c81e2c411d2e420b3466ff048019327
pavelperkin <pavelperkin@pavelperkins-MacBook-Pro.local>
2014-02-03 13:03:38 +0200

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+IdleBlog::Application.config.secret_key_base = '249784b537f06f7af2685348d8225c32f48fa34be4daf45d472830b7785c1d9f633b8f1d748df2a541fcea2b1c3e0f4e59e4267b27e47201408f11eed80debf6'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+IdleBlog::Application.config.session_store :cookie_store, key: '_Idle_Blog_session'
