b0d59484554342a338c5b70ec839749562743978
pavelperkin <pavelperkin@pavelperkins-MacBook-Pro.local>
2014-02-03 14:06:33 +0200

    user has many posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,7 @@
 class PostsController < ApplicationController
+  before_filter :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
   before_action :set_post, only: [:show, :edit, :update, :destroy]
+
 
   # GET /posts
   # GET /posts.json
